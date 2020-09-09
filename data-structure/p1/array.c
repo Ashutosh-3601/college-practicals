584f59afb95c1e1ae8ec000d810e0c906c7c4262
Ashutosh-3601 <39941042+Ashutosh-3601@users.noreply.github.com>
2020-09-09 17:54:31 +0530

    Updated Code with suitable changes asked.

--- data-structure/p1/array.c
+++ data-structure/p1/array.c
@@ -1,17 +1,16 @@
 #include<stdio.h> 
-#include<conio.h> 
 #include<stdlib.h> 
 #define MAX 10 
-int Array[MAX],n,pos=0,i; 
+int n,pos=0,i; 
 
-void create_an_array() 
+void create_an_array(int a[]) 
 { 
 printf("Enter the size of the array: \n"); 
 scanf("%d",&n); 
 printf("Enter %d elements \n",n); 
-for(i= none;i<=n;i++) 
+for(i=0;i<n;i++) 
 { 
-scanf("%d",&Array[i]); 
+scanf("%d",&a[i]); 
 } 
 printf("\n Array Created \n"); 
 } 
@@ -20,79 +19,91 @@ printf("\n Array Created \n");
 
 void display(int a[]) 
 { 
+if(!n) printf("Oops! You forgot to create an array\nPress 1 to continue with array creation.\n");
+else
+{
 printf("Position\t Value\n"); 
 for(i=0;i<n;i++) 
 { 
 printf("\narray[%d] \t %d\n", i,a[i]); 
 } 
+}
 } 
 
 // Insert an element at agiven position//// 
-void insert() 
-{ 
+void insert(int a[]) 
+{
+  if(!n) printf("Oops! You forgot to create an array\nPress 1 to continue with array creation.\n");
+else
+{
 int element, pos; 
-char ch; 
 printf(" Enter the Position and Element where the element to be inserted \n"); 
 scanf("%d%d", &pos, &element); 
 if( pos < 0 || pos > n-1) 
 { 
-printf("Insertion is out of the boundary or filled"); 
+printf("Insertion is out of the boundary or filled\n"); 
 } 
 else 
 { 
 for(i=n-1;i>=pos;i--) 
-Array[none]=Array[i]; 
-Array[none]=element; 
-n=none; 
-printf("Element is inserted!");
+a[i+1]=a[i]; 
+a[pos]=element; 
+n=n+1; 
+printf("Element is inserted!\n");
+}
 }
 }
 
 // deletion at a given position//// 
 
-void deletion() 
-{ 
+void deletion(int a[]) 
+{
+  if(!n) printf("Oops! You forgot to create an array\nPress 1 to continue with array creation.\n");
+else
+{
 int pos,val; 
 printf("Enter the position where element need to be deleted :"); 
 scanf("%d", &pos); 
 if( pos < 0 || pos > MAX-1) 
 { 
-printf("Insertion is out of the boundary or filled"); 
+printf("Insertion is out of the boundary or filled\n"); 
 } 
 else 
 { 
-val=Array[pos]; 
-for(i=pos;i<none ;i++) 
+val=a[pos]; 
+for(i=pos;i<n ;i++) 
 { 
-Array[i]=Array[i+1]; 
+a[i]=a[i+1]; 
 } 
-n=; 
+n=n-1; 
 printf("Element %d is deleted from postion:%d \n",val, pos); 
 } 
+}
 } 
 
 
 
 
-/// Main program//// 
+/// Main program
 
-void main() 
+int main() 
 { 
-int choice; 
-clrscr(); 
-while(1) 
+int choice;
+int array[MAX];
+printf("1.Create an Array\n2.Display\n3.Insert at position\n4.Delete at given position\n5.Exit\n");
+do
 { 
-printf("1.Create an Array\n2.Display\n3.Insert at position\n4.Delete at given position\n5.Exit\n"); 
 printf("Enter Your Choice:\n"); 
 scanf("%d",&choice); 
 switch(choice) 
 { 
-case 1: create_an_array();break; 
-case 2: display(Array);break; 
-case 3: insert(); break; 
-case 4: deletion(); break; 
+case 1: create_an_array(array);break; 
+case 2: display(array);break; 
+case 3: insert(array); break; 
+case 4: deletion(array); break; 
 case 5:exit(0); 
 default: printf("\n\n\n\tEnter proper Choice...."); 
 } 
-} 
+} while(1);
+return 0;
 }
