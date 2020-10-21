c2a2165d098f1eaf36b20f010e5ded54291a4f62
Ashutosh-3601 <39941042+Ashutosh-3601@users.noreply.github.com>
2020-10-22 00:04:11 +0530

    Modified code

--- data-structure/p5/towers-of-hanoi.c
+++ data-structure/p5/towers-of-hanoi.c
@@ -1,12 +1,26 @@
-#include<stdio.h> 
-void towers(int, char, char, char); void main() 
+#include<stdio.h>
+#include <time.h>
+
+void towers(int, char, char, char);
+int main() 
 { 
 int num; 
 printf("enter the number of disks : "); 
 scanf("%d", &num); 
 printf("the sequence of moves involved in the ower of honai are : \n");
+
+  /* clock structure variables*/
+clock_t start, end;
+double cpu_time_used;
+/* Start the clock */
+start = clock();
+
 towers(num, 'A','C','B'); 
-getch(); 
+
+/* Stop the clock after returning from function*/
+end = clock();
+cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+printf("Calculating took %f seconds to execute \n", cpu_time_used);
 } 
 
 void towers(int num, char frompeg, char topeg, char auxpeg) 
@@ -16,7 +30,7 @@ if(num == 1)
 printf("\n MOVE DISK 1 FROM PEG %c TO PEG %c", frompeg, topeg); 
 return; 
 } 
-towers( num -1, frompeg, none ,topeg); 
+towers( num -1, frompeg, auxpeg ,topeg); 
 printf("\n MOVE DISK %d FROM PEG %c TO PEG %c", num,frompeg, topeg);
-towers( num -1, none ,none ,frompeg );
+towers( num -1, auxpeg ,topeg ,frompeg );
 }
