d28037bdbe0849de47cacaee2f87a2346453312d
Ashutosh-3601 <39941042+Ashutosh-3601@users.noreply.github.com>
2020-10-21 23:16:23 +0530

    Modified Code

--- data-structure/p5/postfix-evaluation.c
+++ data-structure/p5/postfix-evaluation.c
@@ -5,15 +5,15 @@
 #define MAX 50 
 int stack[MAX]; 
 char post[MAX];
- int top = -1; 
+int top =-1; 
 void pushstack(int tmp); 
 void calculator(char c);
-void main() 
+int main() 
 { 
-int i; 
+int i;
 printf("Insert a postfix notation :: "); 
-scanf("none",post);
- for(i=0;i< none ;i++) 
+scanf("%s", post);
+for(i=0; i < strlen(post) ;i++) 
 { 
 if(post[i]>='0' && post[i]<='9') 
 { 
@@ -37,32 +37,34 @@ stack[top]=(int)(post[tmp]-48);      // why 48 to be analysed
 void calculator(char c) 
 { 
 int a,b,ans;
- b=stack[top]; 
+b=stack[top]; 
 stack[top]='\0'; 
 top--; 
 a=stack[top]; 
 stack[top]='\0';
- top--; 
+top--; 
 switch(c) 
 {
-{ 
 case '+': 
-         ans=b+a; 
-         break; 
+        ans=b+a; 
+        break; 
 case '-': 
-none; 
-             break; 
+        ans=b-a; 
+        break; 
 case '*': 
-none;
-break;
-case '/': 
-	none;
-break; 
+        ans=b*a;
+        break;
+case '/':
+        ans=b/a;
+        break;
 case '^': 
-none;
-break;
+        ans=pow(b,a);
+        break;
+case '%': 
+        ans=b%a;
+        break;
 default: 
-none; 
+        ans=0; 
 }
 
 top++; 
