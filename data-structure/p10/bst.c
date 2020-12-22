eb37bd262baea6ced0992c51dc3cf6d2ccfd67e8
Ashutosh-3601 <39941042+Ashutosh-3601@users.noreply.github.com>
2020-12-22 22:42:44 +0530

    updates

--- data-structure/p10/bst.c
+++ data-structure/p10/bst.c
@@ -1,122 +1,173 @@
-#include <stdio.h>
-#include <stdlib.h>
-struct BST
+#include<stdio.h>
+#include<stdlib.h>
+struct node
 {
-int data;
-struct BST *left;
-struct BST *right;
+int info;
+struct node *llink;
+struct node *rlink;
 };
-typedef struct BST NODE;
-NODE *node;
-NODE* createtree(NODE *node, int data)
+typedef struct node *NODE;
+//Function to create a BST of N integers:6 9 5 2 8 15 24 14 7 8 5 2
+NODE insert_node(int item,NODE root)
 {
-if (node == NULL)
-{
-NODE *temp;
-temp= (NODE*)malloc(sizeof(NODE));
-temp->data = data;
-temp->left = temp->right = NULL;
+NODE cur,temp,prev;
+temp=(NODE)malloc(sizeof(struct node));
+temp->info=item;
+temp->llink=temp->rlink=NULL;
+if(root==NULL)
 return temp;
-}
-if (data < (node->data))
+prev=NULL;
+cur=root;
+while(cur!=NULL)
 {
-node->left = createtree(node->left, data);
-}
-else if (data >= node->data)
-{
-node -> right = createtree(node->right, data);
-}
-return node;
-}
-
-NODE* search(NODE *node, int data)
-{
-if(node == NULL)
-printf("\nElement not found");
-else if(data < node->data)
-{
-node->left=search(node->left, data);
-}
-else if(data >= node->data)
-{
-node->right=search(node->right, data);}
+prev=cur;
+if(item<cur->info)
+cur=cur->llink;
+else if(item>cur->info)
+cur=cur->rlink;
 else
-printf("\nElement found is: %d", node->data);
-return node;
-}
-
-void inorder(NODE *node)
 {
-if(node != NULL)
-{
-inorder(node->left);
-printf("%d\t", node->data);
-inorder(node->right);
+printf("Item already exists cant insert\n");//if duplicates items
+free(temp);
+return root;
 }
 }
-void preorder(NODE *node)
-{
-if(node != NULL)
-{
-printf("%d\t", node->data);
-preorder(node->left);
-preorder(node->right); 
+if(item<prev->info)
+prev->llink=temp;
+else
+prev->rlink=temp;
+return root;
 }
+void inorder(NODE root)
+{
+if(root==NULL)
+return;
+inorder(root->llink);
+printf("%d\t",root->info);
+inorder(root->rlink);
 }
-void postorder(NODE *node)
+void preorder(NODE root)
 {
-if(node != NULL)
-{
-postorder(node->left);
-postorder(node->right);
-printf("%d\t", node->data);
+if(root==NULL)
+return;
+printf("%d\t",root->info);
+preorder(root->llink);
+preorder(root->rlink);
 }
+void postorder(NODE root)
+{
+if(root==NULL)
+return;
+postorder(root->llink);
+postorder(root->rlink);
+printf("%d\t",root->info);
 }
-
-void main()
+NODE search(int item,NODE root)
 {
-int data, ch, i, n;
-NODE *root=NULL;
-clrscr();
-while (1)
-{
-printf("\n1.Insertion in Binary Search Tree");
-printf("\n2.Search Element in Binary Search Tree");
-printf("\n3.Delete Element in Binary Search Tree");
-printf("\n4.Inorder\n5.Preorder\n6.Postorder\n7.Exit");
-printf("\nEnter your choice: ");
-scanf("%d", &ch);
-switch (ch)
-{
-case 1: printf("\nEnter N value: " );
-scanf("%d", &n);
-printf("\nEnter the values to create BST like(6,9,5,2,8,15,24,14,7,8,5,2)\n");
-for(i=0; i<n; i++)
-{
-scanf("%d", &data);
-root=createtree(root, data);
+if(root==NULL||item==root->info)
+return root;
+if(item<root->info)
+return search(item,root->llink);
+return search(item,root->rlink);
 }
+NODE delete_node(int item,NODE root)
+{
+NODE child,suc,par,cur;
+//if tree is empty
+if(root==NULL)
+{
+printf("Tree is empty\n");
+return root;
+}
+//tree is existing
+par=NULL;
+cur=root;
+while(cur!=NULL&&item!=cur->info)
+{
+par=cur;
+if(item<cur->info)
+cur=cur->llink;
+else
+cur=cur->rlink;
+}
+if(cur==NULL)
+{
+printf("Item not found\n");
+return root;
+}
+//node to be delted has only one subtree
+if(cur->llink==NULL)
+child=cur->rlink;
+else if(cur->rlink==NULL)
+child=cur->llink;
+//node to be deleted has both subtrees
+else
+{
+child=cur->rlink;
+suc=cur->rlink;
+while(suc->llink!=NULL)
+{
+suc=suc->llink;
+}
+suc->llink=cur->llink;
+}
+if(par==NULL)
+{
+free(cur);
+return child;
+}
+//Node to be deleted has no children
+if(cur==par->llink)
+par->llink=child;
+else
+par->rlink=child;
+free(cur);
+return root;
+}
+int main()
+{
+int ch,item;
+NODE root=NULL,temp,parent;
+for(;;)
+{
+printf("\n1.Create\n");
+printf("2.Traverse the tree in Preorder, Inorder and Postorder \n");
+printf("3.Search\n");
+printf("4.Delete an element from the tree\n");
+printf("5.Exit\n");
+printf("Enter your choice\n");
+scanf("%d",&ch);
+switch(ch)
+{
+case 1: printf("Enter the item to be inserted\n");
+scanf("%d",&item);
+root=insert_node(item,root);
 break;
-case 2: printf("\nEnter the element to search: ");
-scanf("%d", &data);
-root=search(root, data);
-break;
-case 3: printf("\nEnter the element to delete: ");
-scanf("%d", &data);
-root=del(root, data);
-break;
-case 4: printf("\nInorder Traversal: \n");
+case 2:if(root==NULL)
+printf("\nTree is not created\n");
+else
+{
+printf("\nThe Inorder display:\n");
 inorder(root);
-break;
-case 5: printf("\nPreorder Traversal: \n");
+printf("\nThe Preorder display:\n");
 preorder(root);
-break;
-case 6: printf("\nPostorder Traversal: \n");
+printf("\nThe Postorder display:\n");
 postorder(root);
-break;
-case 7: exit(0);
-default:printf("\nWrong option");
-break; 
 }
-} 
+break;
+case 3:printf("\nEnter Elements to be searched\n");
+scanf("%d",&item);
+temp=search(item,root);
+if(temp==NULL)
+printf("\nElement does not exists\n");
+else
+printf("\nThe Element %d is found\n",temp->info);
+break;
+case 4:printf("\nEnter the Element to be deleted\n");
+scanf("%d",&item);
+root=delete_node(item,root);
+break;
+default:exit(0);
+}
+}
 }
