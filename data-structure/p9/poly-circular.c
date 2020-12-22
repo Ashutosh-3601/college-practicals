18108769e2d9f2765276742a1e3687af2f8667e3
Ashutosh-3601 <39941042+Ashutosh-3601@users.noreply.github.com>
2020-12-22 22:15:17 +0530

    changes done

--- data-structure/p9/poly-circular.c
+++ data-structure/p9/poly-circular.c
@@ -1,187 +1,185 @@
-#include<stdio.h> 
-#include<conio.h> 
-#include<alloc.h> 
-#include<process.h> 
-#include<math.h> 
-struct node 
-{ 
-float cf; 
-int px,py,pz; 
-int flag; 
-struct node *link; 
-}; 
-typedef struct node *NODE; 
-NODE getnode() 
-{ 
-NODE x; 
-x=(NODE) malloc (sizeof(struct node));  
-if(x==NULL) 
-{ 
-printf("out of memory\n"); 
-exit(0); 
-} 
-return x; 
-} 
-NODE insert_rear(float cf,float x , float y, float z,NODE head) 
-{ 
-NODE temp,cur; 
-temp=getnode(); 
-temp->cf=cf; 
-temp->px=x; 
-temp-> py=y; 
-temp->pz=z; 
-temp->flag=0; 
-cur=head->link; 
-while(cur->link!=head) 
-{ 
-cur=cur->link;} 
-cur->link=temp;
-temp->link=head; 
-return head; 
-} 
-void display(NODE head) 
-{ 
-NODE temp; 
-if(head->link==head) 
-{ 
-printf("polynominal doesn't exsits\n"); 
-return; 
-} 
-temp=head->link; 
-while(temp!=head) 
-{ 
-printf("+ % 5.2fx^%dy^%dz^%d",temp->cf,temp->px,temp->py,temp->pz); temp=temp->link; 
-} 
-printf("\n"); 
-} 
-NODE add_poly(NODE h1,NODE h2, NODE h3) 
-{ 
-NODE p1,p2; 
-int x1,x2,y1,y2,z1,z2,cf1,cf2,cf;  
-p1=h1->link;  
-while(p1!=h1) 
-{ 
-x1=p1->px;  
-y1=p1->py; 
-z1=p1->pz; 
-cf1=p1->cf;  
-p2=h2->link;  
-while(p2!=h2) 
-{ 
-x2=p2->px; 
-y2=p2->py; 
-z2=p2->pz; 
-cf2=p2->cf; 
-if(x1==x2 && y1==y2 && z1==z2) break; 
-p2=p2->link; 
-} 
-if(p2!=h2) 
-{ 
-cf=cf1+cf2; 
-p2->flag=1; 
-if(cf!=0) 
-h3=insert_rear(cf,x1,y1,z1,h3);
-} 
-else 
-h3=insert_rear(cf1,x1,y1,z1,h3); 
-p1=p1->link; 
-} 
-p2=h2->link; 
-while(p2!=h2) 
-{  
- if(p2->flag==0) 
- { 
- h3=insert_rear(p2->cf,p2->px,p2->py,p2->pz,h3);  } 
-p2=p2->link; 
-} 
-return h3; 
-} 
-NODE read_poly(NODE head) 
-{ 
-int i,n;  
-int px,py,pz;  
-float cf; 
-printf("enter the number of terms\n"); 
-scanf("%d",&n); 
-for(i=1;i<=n;i++) 
-{ 
-printf("enter the %d term\n",i); 
-printf("coeff="); 
-scanf("%f",&cf); 
-//if(cf==-999) break; 
-printf("pow x="); 
-scanf("%d",&px); 
-printf("pow y="); 
-scanf("%d",&py); 
-printf("pow z="); 
-scanf("%d",&pz); 
-head=insert_rear(cf,px,py,pz,head);  
-} 
-return head; 
-} 
-void polysum() 
-{ 
-NODE h1,h2,h3; 
-h1=getnode(); 
-h2=getnode(); 
-h3=getnode(); 
-h1->link=h1;
-h2->link=h2; 
-h3->link=h3; 
-printf("enter the first polynominal\n"); 
-h1=read_poly(h1); 
-printf("enter the second polynominal\n"); 
-h2=read_poly(h2); 
-h3=add_poly(h1,h2,h3); 
-printf(" the first polynominal is\n"); 
-display(h1); 
-printf("second polynominal is\n"); 
-display(h2); 
-printf("the sum of two polynominal is\n"); 
-display(h3); 
-} 
-void represent_evaluate() 
-{ 
-NODE e1,temp; 
-int x,y,z; 
-float sum=0.0; 
-e1=getnode(); 
-e1->link=e1; 
-printf("enter the polynominal\n"); 
-e1=read_poly(e1); 
-printf("polynominal i s \n"); 
-display(e1); 
-printf("enter the values of coefficient\n"); 
-scanf("%d%d%d",&x,&y,&z); 
- if(e1==NULL) 
- { 
- printf("list is empty"); 
- } 
- else 
- { 
-temp=e1->link;  
-while(temp!=e1)  
-{  
-sum+=temp->cf*pow(x,temp->px)*pow(y,temp->py)*pow(z,temp->pz);  temp=temp->link;  
-}  
- // sum+=temp->cf*pow(x,temp->px)*pow(y,temp->py)*pow(z,temp->pz);  printf("the total sum is %f\n",sum);  
-}  
-return; 
-} 
-void main() 
-{ 
-int choice; 
-clrscr();  
-while(1) 
-{  
-printf("\n\n\n\t1.represent and evaluate...\t2.ADD TWO poly..\t3.Exit...");  printf("\n\n\n\tEnter Your Choice: ");  
-scanf("%d",&choice);  
-switch(choice)  
-{  
- case 1: represent_evaluate();break;  
- case 2:polysum();break;  
- case 3:exit(0);  
- default: printf("\n\n\n\tEnter proper Choice....");  
- } 
- } 
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+struct node
+{
+ int cf;
+ int px;
+ int py;
+ int pz;
+ struct node *link; //self referencial structure
+};
+typedef struct node* NODE;
+void display(NODE head)
+{
+ NODE cur;
+ if(head->link==head)
+ {
+ printf("Polynomial does not exists\n");
+ return;
+ }
+ cur=head->link;
+ while(cur!=head)
+ {
+ if(cur->cf>0)
+ printf("+");
+ printf("%d",cur->cf);
+ printf("x^%d",cur->px);
+ printf("y^%d",cur->py);
+ printf("z^%d",cur->pz);
+ cur=cur->link;
+ }
+}
+NODE insert_rear(int cf,int px,int py,int pz,NODE head)
+{
+ NODE temp,cur;
+ temp=(NODE)malloc(sizeof(struct node));
+ temp->cf=cf;
+ temp->px=px;
+ temp->py=py;
+ temp->pz=pz;
+ temp->link=temp;
+ cur=head->link;
+ while(cur->link!=head)
+ {
+ cur=cur->link;
+ }
+ cur->link=temp;
+ temp->link=head;
+ return head;
+}
+NODE readpoly(NODE head)
+{
+ int n,i,cf,px,py,pz;
+ printf("enter the number of terms\n");
+ scanf("%d",&n);
+ for(i=1;i<=n;i++)
+ {
+ printf("\nenter the coefficients of %dterm\n",i);
+ scanf("%d",&cf);
+
+ printf("\nenter the power of x of %d term\n",i);
+ scanf("%d",&px);
+ printf("\nenter the power of y of %d term\n",i);
+ scanf("%d",&py);
+ printf("enter the power of z of %d term\n",i);
+ scanf("%d",&pz);
+ head=insert_rear(cf,px,py,pz,head);
+ }
+ return head;
+}
+void evaluate(NODE head)
+{
+ NODE cur;
+ int x,y,z,result=0;
+ if(head->link==head)
+ {
+ printf("Polynomial does not exists\n");
+ return;
+ }
+ printf("\nEnter the value of x\n");
+ scanf("%d",&x);
+ printf("Enter the value of y\n");
+ scanf("%d",&y);
+ printf("Enter the value of z\n");
+ scanf("%d",&z);
+ cur=head->link;
+ while(cur!=head)
+ {
+ result=result+cur->cf*pow(x,cur->px)*pow(y,cur->py)*pow(z,cur->pz);
+ cur=cur->link;
+ }
+ printf("The result of evaluation is %d\n",result);
+}
+NODE addpoly(NODE h1,NODE h2,NODE h3)
+{
+ NODE p1,p2;
+ p1=h1->link;
+ p2=h2->link;
+ int sumcf;
+ while(p1!=h1&&p2!=h2)
+ {
+ if(p1->px > p2->px)
+ {
+ h3=insert_rear(p1->cf,p1->px,p1->py,p1->pz,h3);
+ p1=p1->link;
+ }
+ else if(p1->px < p2->px)
+ {
+ h3=insert_rear(p2->cf,p2->px,p2->py,p2->pz,h3);
+ p2=p2->link;
+ }
+ else
+ {
+ if(p1->py > p2->py)
+ {
+ h3=insert_rear(p1->cf,p1->px,p1->py,p1->pz,h3);
+ p1=p1->link;
+ }
+ else if (p1->py < p2->py)
+ {
+
+ h3=insert_rear(p2->cf,p2->px,p2->py,p2->pz,h3);
+ p2=p2->link;
+ }
+ else
+ {
+ if(p1->pz > p2->pz)
+ {
+ h3=insert_rear(p1->cf,p1->px,p1->py,p1->pz,h3);
+ p1=p1->link;
+ }
+ else if (p1->pz < p2->pz)
+ {
+
+ h3=insert_rear(p2->cf,p2->px,p2->py,p2->pz,h3);
+ p2=p2->link;
+ }
+ else
+ {
+ sumcf=p1->cf+p2->cf;
+ if(sumcf!=0)
+ h3=insert_rear(sumcf,p1->px,p1->py,p1->pz,h3);
+ p1=p1->link;
+ p2=p2->link;
+ }
+ }
+ }
+ }
+while(p1!=h1)
+{
+ h3=insert_rear(p1->cf,p1->px,p1->py,p1->pz,h3);
+ p1=p1->link;
+}
+while(p2!=h2)
+{
+ h3=insert_rear(p2->cf,p2->px,p2->py,p2->pz,h3);
+ p2=p2->link;
+}
+return h3;
+}
+int main()
+{
+ NODE h1,h2,h3;
+ h1=(NODE)malloc(sizeof(struct node));
+ h2=(NODE)malloc(sizeof(struct node));
+ h3=(NODE)malloc(sizeof(struct node));
+ h1->link=h1;
+ h2->link=h2;
+ h3->link=h3;
+ printf("enter the first polynomial\n");
+ h1=readpoly(h1);
+ printf("enter the Second polynomial\n");
+ h2=readpoly(h2);
+ h3=addpoly(h1,h2,h3);
+ printf("The first polynomial is \n");
+ display(h1);
+ evaluate(h1);
+
+ printf("The Second polynomial is \n");
+ display(h2);
+ evaluate(h2);
+ printf("The Sum is \n");
+ display(h3);
+ evaluate(h3);
 }
