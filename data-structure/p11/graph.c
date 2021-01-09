6f7fd0edf8801956cfb60f493f89488684526504
Ashutosh-3601 <39941042+Ashutosh-3601@users.noreply.github.com>
2021-01-09 22:30:34 +0530

    Create graph.c

---
+++ data-structure/p11/graph.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+int a[20][20],q[20],visited[20],reach[10],n,i,j,f=0,r=-1,count=0,flag=0;
+void bfs(int v)
+{
+for(i=1;i<=n;i++)
+if(a[v][i] && !visited[i])
+q[++r]=i;
+if(f<=r)
+{
+visited[q[f]]=1;
+bfs(q[f++]);
+}
+}
+void dfs(int v)
+{
+int i;
+reach[v]=1;
+for(i=1;i<=n;i++)
+{
+if(a[v][i] && !reach[i])
+{
+printf("\n %d->%d",v,i);
+count++;
+dfs(i);
+}
+}
+}
+int main()
+{
+int v, choice;
+printf("\n Enter the number of vertices:");
+scanf("%d",&n);
+printf("\n Enter graph data in matrix form:\n");
+for(i=1;i<=n;i++)
+for(j=1;j<=n;j++)
+scanf("%d",&a[i][j]);
+for(;;)
+{
+printf("\n 1.BFS\n 2.DFS\n 3.Exit\n");
+printf("Enter your choice : ");
+
+scanf("%d",&choice);
+switch(choice)
+{
+case 1: printf("\n Enter the starting vertex:");
+scanf("%d",&v);
+for(i=1;i<=n;i++)
+{
+q[i]=0;
+visited[i]=0;
+}
+bfs(v);
+if((v<1)||(v>n))
+{
+printf("\n Bfs is not possible");
+}
+else
+{
+printf("\n The nodes which are reachable from %d: ",v);
+for(i=1;i<=n;i++)
+if(visited[i])
+printf("%d\t",i);
+}
+for(i=1;i<=n;i++)
+{
+if(visited[i])
+{
+flag=1;
+}
+}
+if(flag==0)
+printf("\nFrom %d no vertex can be reached\n",v);
+break;
+case 2:
+for(i=1;i<=n-1;i++)
+reach[i]=0;
+dfs(1);
+if(count==n-1)
+printf("\n Graph is connected");
+else
+printf("\n Graph is not connected");
+break;
+case 3: exit(0);
+}
+}
+}
